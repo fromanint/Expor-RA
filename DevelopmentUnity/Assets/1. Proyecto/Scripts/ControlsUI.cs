77a3da8ebbd6be13539e6de2a9499cd3b18527aa
fromanint <RomanInteractive@FROMAN.local>
2017-04-04 12:57:32 -0500

    add control script

--- DevelopmentUnity/Assets/1. Proyecto/Scripts/ControlsUI.cs
+++ DevelopmentUnity/Assets/1. Proyecto/Scripts/ControlsUI.cs
@@ -1,10 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlsUI : MonoBehaviour {
 
-    Transform Augmented;
+	RectTransform Augmented;
 
+	public void HorizontalMove(float value)
+	{
+		Vector3 pos = Augmented.position;
+		pos.x += value;
+		Augmented.position = pos;
+	}
+
+	public void VerticalMove(float value)
+	{
+		Vector3 pos = Augmented.position;
+		pos.y += value;
+		Augmented.position = pos;
+	}
+
+	public void Zoom(float value)
+	{
+		Vector3 pos = Augmented.position;
+		pos.z += value;
+		Augmented.position = pos;
+	}
 
 }
